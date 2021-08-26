034459fa8122657efab67f46666e3cdf2f9d1f0d
Manuel Lopez <mp.arturolopez@gmail.com>
2021-08-26 10:33:40 -0400

    HttpRedirection removed

--- api/Startup.cs
+++ api/Startup.cs
@@ -75,12 +75,12 @@ namespace api
                 app.UseDeveloperExceptionPage();
                 
             }
-            app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             app.UseCors("ClientPermission");
             
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
